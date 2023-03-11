3ff94400605655c13d8d4657ffb26c51e7775e04
Wenxing Gao <cherrytree33@sina.com>
2023-03-11 21:12:01 +0800

    Update 11b_Microbial_network.sh

--- scripts/11b_Microbial_network.sh
+++ scripts/11b_Microbial_network.sh
@@ -59,6 +59,6 @@ mkdir ${workplace}bootstrap_feat
 fastspar_bootstrap --otu_table $workplace$inputfile --number 1000 --prefix ${workplace}bootstrap_feat/feat
 
 mkdir ${workplace}$outputfile
-parallel fastspar --otu_table $workplace$inputfile --correlation $workplace$outputfile/cor_{/} --covariance $workplace$outputfile/cov_{/} -i 5 ::: ${workplace}bootstrap_feat/*
+parallel fastspar --otu_table {} --correlation $workplace$outputfile/cor_{/} --covariance $workplace$outputfile/cov_{/} -i 5 ::: ${workplace}bootstrap_feat/*
 
 fastspar_pvalues --otu_table $workplace$inputfile --correlation ${workplace}median_correlation.tsv --prefix $workplace$outputfile/cor_ --permutations 1000 --outfile ${workplace}pvalues.tsv
